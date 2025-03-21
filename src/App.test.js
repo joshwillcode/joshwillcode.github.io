70c5832d88f82eeb405225c24b55c9f802b361aa
Joshua Williams <72379703+joshwillcode@users.noreply.github.com>
2025-03-21 12:41:31 -0700

    Clear out to blank slate, remove create-react-app boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-describe( "A basic set of proof of concept tests for the default redux template", () => {
-  it('Has instructions to edit src/App.js', () => {
+describe( "Test mockup", () => {
+  it('Displays Blank App', () => {
     render(<Provider store={store}> 
       <App/>
     </Provider>);
-    const instructText = screen.getByText('src/App.js');
-    expect(instructText).toBeInTheDocument();
+    const text = screen.getByText('Blank App');
+    expect(text).toBeInTheDocument();
   });
   it 
 });
