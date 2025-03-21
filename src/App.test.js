f63df52429966f71ed8c2b29406b8d0f3fb2904a
Joshua Williams <72379703+joshwillcode@users.noreply.github.com>
2025-03-21 12:07:05 -0700

    Add tests to verify test framework is connected

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe( "A basic set of proof of concept tests for the default redux template", () => {
+  it('Has instructions to edit src/App.js', () => {
+    render(<Provider store={store}> 
+      <App/>
+    </Provider>);
+    const instructText = screen.getByText('src/App.js');
+    expect(instructText).toBeInTheDocument();
+  });
+  it 
 });
