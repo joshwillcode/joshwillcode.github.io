6e1a5135888bf2028e4e56fe35e716e1e8808d75
Joshua Williams <72379703+joshwillcode@users.noreply.github.com>
2025-04-02 15:32:49 -0700

    Add additional cases to Time.test.js, and correctly clean up mocked system time,  add functionality to time display for hours and days

--- src/features/post/time/Time.js
+++ src/features/post/time/Time.js
@@ -8,12 +8,36 @@ function Time({ timestamp }) {
     const currentTime = Date.now()/1000;
     const timeDifference = currentTime - timestamp;
 
+    // second count constants
+    const MINUTE = 60;
+    const HOUR = 3600;
+    const DAY = 86400;
+
+    // time helper function
+    const timeUnit = (seconds, unit) => {
+        return Math.trunc(seconds/unit);
+    }
+
     // determine how long ago post was made
-    if (timeDifference < 60) {
+    if (timeDifference < MINUTE) {
         timeDisplay = 'just now';
-    };
+    } else if (timeDifference < MINUTE*2) {
+        timeDisplay = 'a minute ago';
+    } else if (timeDifference < HOUR) {
+        timeDisplay = `${timeUnit(timeDifference, MINUTE)} minutes ago`;
+    } else if (timeDifference < HOUR*2) {
+        timeDisplay = 'an hour ago';
+    } else if (timeDifference < DAY) {
+        timeDisplay = `${timeUnit(timeDifference, HOUR)} hours ago`;
+    } else if (timeDifference < DAY*2) {
+        timeDisplay = 'a day ago';
+    } else if (timeDifference) {
+        timeDisplay = `${timeUnit(timeDifference, DAY)} days ago`;
+    }
 
-
+    if(!timestamp) {
+        timeDisplay = 'no timestamp passed in';
+    }
 
     return(
         <div>
--- src/features/post/time/Time.test.js
+++ src/features/post/time/Time.test.js
@@ -5,9 +5,14 @@ import Time from './Time';
 describe('Takes in unix timestamp, renders a description of the time in comparison to the current time', () => {
     describe("displays 'just now' when time difference is less than 1 minute", () => {
         
-        // mock system time to 4/1/2025 12:00pm PST
-        jest.useFakeTimers();
-        jest.setSystemTime(new Date('2025-04-01T00:19:00').getTime());
+        beforeAll(() => {
+            // mock system time to 4/1/2025 12:00pm PST
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1743534000*1000).getTime());
+        })
+        afterAll(() => {
+            jest.useRealTimers();
+        })
 
         it("Displays just now", () => {
             // all times on 4/1/2025 PST
@@ -25,67 +30,117 @@ describe('Takes in unix timestamp, renders a description of the time in comparis
             expect(text).not.toBeInTheDocument();
         });
     });
-    // describe("Displays 'x minutes ago' (rounded) when time difference is less than 60 minutes", () => {
-    //     it("displays '30 minutes ago'", () => {
-    //         // 11:30am -> 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>)
-    //         screen.getByText('30 minutes ago');
+    describe("Displays 'x minutes ago' (rounded) when time difference is less than 60 minutes", () => {
+        
+        beforeAll(() => {
+            // mock system time to 4/1/2025 12:00pm PST
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1743534000*1000).getTime());
+        })
+        afterAll(() => {
+            jest.useRealTimers();
+        })
 
-    //         // 11:30:59 -> 12:00pm
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('30 minutes ago');
-    //     });
-    //     it("displays '59 minutes ago'", () => {
-    //         // 11:01am -> 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>)
-    //         screen.getByText('59 minutes ago');
-    //     });
-    // });
-    // describe("Displays 'x hours ago' when time difference is greater than 60 minutes", () => {
-    //     it("displays 'an hour ago' when time difference is between 60 and 119 minutes", () => {
-    //         // 11:00am -> 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>)
-    //         screen.getByText('an hour ago');
-    //         // 10:30am -> 12:00pm
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('an hour ago');
-    //         // 10:01am -> 12:00pm
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('an hour ago');
-    //     });
-    //     it("displays 'x hours ago' when time difference is 120 minutes or greater", () => {
-    //         // 10:00am -> 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>);
-    //         screen.getByText('2 hours ago');
+        it("displays 'a minute ago'", () => {
+            // 11:59am -> 12:00pm 
+            const {rerender} = render(<Time timestamp='1743533940'/>)
+            screen.getByText('a minute ago');
+            // 11:58:01am -> 12:00pm
+            rerender(<Time timestamp='1743533881'/>)
+            screen.getByText('a minute ago');
+        });
+        it("doesn't display 'a minute ago'", () => {
+            // 11:58am -> 12:00pm
+            render(<Time timestamp='1743533880'/>)
+            const text = screen.queryByText('a minute ago');
+            expect(text).not.toBeInTheDocument();
+        });
+        it("displays '30 minutes ago'", () => {
+            // 11:30am -> 12:00pm 
+            const {rerender} = render(<Time timestamp='1743532200'/>)
+            screen.getByText('30 minutes ago');
 
-    //         const text = screen.queryByText('an hour ago');
-    //         expect(text).not.toBeInTheDocument();
+            // 11:29:01 -> 12:00pm
+            rerender(<Time timestamp='1743532141'/>)
+            screen.getByText('30 minutes ago');
+        });
+        it("displays '59 minutes ago'", () => {
+            // 11:00:01am -> 12:00pm 
+            render(<Time timestamp='1743530401'/>)
+            screen.getByText('59 minutes ago');
+        });
 
-    //         // 9:00am -> 12:00pm
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('3 hours ago');
-    //     });
-    // });
-    // describe("It displays 'x days ago' when time difference is 24 hours or greater", () => {
-    //     it("displays 'a day ago' when time difference is between 24 hours and 48 hours", () => {
-    //         // 4th 12:00pm ->  5th, 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>);
-    //         screen.getByText('a day ago');
 
-    //         // 3rd 12:00:01pm -> 5th, 12:00pm 
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('a day ago');
-    //     });
-    //     it("displays 'x days ago' when time difference is above 48 hours", () => {
-    //         // 4th 12:00pm ->  5th, 12:00pm 
-    //         const {rerender} = render(<Time timestamp=''/>);
-    //         screen.getByText('a day ago');
+    });
+    describe("Displays 'x hours ago' when time difference is greater than 60 minutes", () => {
 
-    //         // 3rd 12:00:01pm -> 5th, 12:00pm 
-    //         rerender(<Time timestamp=''/>)
-    //         screen.getByText('a day ago');
-    //     });
-    // });
+        beforeAll(() => {
+            // mock system time to 4/1/2025 12:00pm PST
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1743534000*1000).getTime());
+        })
+        afterAll(() => {
+            jest.useRealTimers();
+        })
+
+        it("displays 'an hour ago' when time difference is between 60 and 119 minutes", () => {
+            // 11:00am -> 12:00pm 
+            const {rerender} = render(<Time timestamp='1743530400'/>)
+            screen.getByText('an hour ago');
+            // 10:30am -> 12:00pm
+            rerender(<Time timestamp='1743528600'/>)
+            screen.getByText('an hour ago');
+            // 10:00:01am -> 12:00pm
+            rerender(<Time timestamp='1743526801'/>)
+            screen.getByText('an hour ago');
+        });
+        it("displays 'x hours ago' when time difference is 120 minutes or greater", () => {
+            // 10:00am -> 12:00pm 
+            const {rerender} = render(<Time timestamp='1743526800'/>);
+            screen.getByText('2 hours ago');
+
+            const text = screen.queryByText('an hour ago');
+            expect(text).not.toBeInTheDocument();
+
+            // 9:00am -> 12:00pm
+            rerender(<Time timestamp='1743523200'/>)
+            screen.getByText('3 hours ago');
+        });
+    });
+    describe("It displays 'x days ago' when time difference is 24 hours or greater", () => {
+
+        beforeAll(() => {
+            // mock system time to 4/1/2025 12:00pm PST
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1743879600*1000).getTime());
+        })
+        afterAll(() => {
+            jest.useRealTimers();
+        })
+
+        it("displays 'a day ago' when time difference is between 24 hours and 48 hours", () => {
+            // 4th 12:00pm ->  5th, 12:00pm 
+            const {rerender} = render(<Time timestamp='1743793200'/>);
+            screen.getByText('a day ago');
+
+            // 3rd 12:00:01pm -> 5th, 12:00pm 
+            rerender(<Time timestamp='1743706801'/>)
+            screen.getByText('a day ago');
+        });
+        it("displays 'x days ago' when time difference is above 48 hours", () => {
+            // 3rd 12:00pm ->  5th, 12:00pm 
+            const {rerender} = render(<Time timestamp='1743706800'/>);
+            screen.getByText('2 days ago');
+
+            // 1st 12:00pm -> 5th, 12:00pm 
+            rerender(<Time timestamp='1743534000'/>)
+            screen.getByText('4 days ago');
+
+            // 1st 12:00:01pm -> 5th, 12:00pm 
+            rerender(<Time timestamp='1743534001'/>)
+            screen.getByText('3 days ago');
+        });
+    });
 
     // TODO create cases for months, and years
     
